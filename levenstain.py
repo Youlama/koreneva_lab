4a038012b053a409936a2d1947f9f470247b3bd5
Sofia Koreneva <sofiakoreneva3195@gmail.com>
2023-12-28 20:36:14 +0300

    Adding lab2.2

---
+++ levenstain.py
@@ -0,0 +1,38 @@
+def levenstain(word1, word2):
+    a = word1
+    b = word2
+    mat = []
+    st = []
+    for i in range(len(a) + 1):
+        for j in range(len(b) + 1):
+            st.append(0)
+        mat.append(st)
+        st = []
+
+    for i in range(len(b) + 1):
+        mat[0][i] = i
+
+    for i in range(len(a) + 1):
+        mat[i][0] = i
+
+    m = 1
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i - 1] == b[j - 1]:
+                m = 0
+            first = mat[i][j - 1] + 1
+            second = mat[i - 1][j] + 1
+            third = mat[i - 1][j - 1] + m
+            mat[i][j] = min(first, second, third)
+            m = 1
+
+    for i in range(len(a) + 1):
+        print(mat[i])
+
+    return mat[len(a)][len(b)]
+
+
+w1 = input("Введите 1 слово: ")
+w2 = input("Введите 2 слово: ")
+
+print(levenstain(w1, w2))
